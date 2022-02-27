0af42a901bae17abb7fad9a6b406d8a994eaf31e
Dan Moneypenny <catullus1973@gmail.com>
2022-02-27 14:38:10 -0800

    update

--- app/app.js
+++ app/app.js
@@ -1,11 +1,8 @@
-/**
- * Required External Modules
- */
-
 const express = require("express");
+const path = require('path');
 const cors = require("cors");
 const helmet = require("helmet");
-const { clientOrigins, serverPort } = require("./config/env.dev");
+const { clientOrigins } = require("./config/env.dev");
 
 const { messagesRouter } = require("./messages/messages.router");
 
@@ -32,6 +29,7 @@ const apiRouter = express.Router();
 app.use(helmet());
 app.use(cors({ origin: clientOrigins }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use("/api", apiRouter);
 
