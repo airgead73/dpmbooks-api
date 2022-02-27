375cbad15a8ac5851bec589d48df4c9b356ef16b
Dan Moneypenny <catullus1973@gmail.com>
2022-02-27 05:36:22 -0800

    add app files

---
+++ app/app.js
@@ -0,0 +1,49 @@
+/**
+ * Required External Modules
+ */
+
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const { clientOrigins, serverPort } = require("./config/env.dev");
+
+const { messagesRouter } = require("./messages/messages.router");
+
+/**
+ * App Variables
+ */
+
+const app = express();
+const apiRouter = express.Router();
+
+/**
+ *  App Configuration
+ */
+
+//  app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "https://dpmsandbox.dev");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
+app.use(helmet());
+app.use(cors({ origin: clientOrigins }));
+app.use(express.json());
+
+app.use("/api", apiRouter);
+
+apiRouter.use("/messages", messagesRouter);
+
+app.use(function (err, req, res, next) {
+  console.log(err);
+  res.status(500).send(err.message);
+});
+
+/**
+ * Server Activation
+ */
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const app = require('./app/app')
+const port = process.env.SERVER_PORT || 6060;
+
+if (port === undefined) {
+  throw new Error(
+    "Port is not defined."
+  );
+}
+
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listenting', onListening);
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  // assign error messages.
+  switch (error.code) {
+    case 'EACCES':
+      console.log(`Port ${port} requires privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.log(`Port ${port} is already in use.`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  console.log(`Mode is ${process.env.NODE_ENV}. Listening on ${port}.`)
+}
